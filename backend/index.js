ff70678fd5998bb65ba32f37f868a030bf000b25
Captain-Aditya-17 <khushimate90@gmail.com>
2025-02-20 21:55:17 +0530

    Ready for deployment

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,10 @@ const connectToDb = require('./db/db.js');
 connectToDb();
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: 'https://solo-anime.vercel.app/',
+    credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
